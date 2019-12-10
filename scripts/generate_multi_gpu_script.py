58a051314b5bf8ff83fda63dd1bbd735b496faff
Xingjian Shi <xshiab@connect.ust.hk>
2019-12-10 00:32:24 -0800

    Create generate_multi_gpu_script.py

---
+++ scripts/generate_multi_gpu_script.py
@@ -0,0 +1,16 @@
+import os
+BASE_DIR = 'run_superglue'
+TASKS = ['commit', 'copa', 'multirc', 'record', 'rte', 'wic', 'wsc',
+         'boolq++', 'commit++', 'copa++', 'rte++',
+         'boolq-bow', 'commit-bow', 'copa-bow', 'multirc-bow', 'record-bow',
+         'rte-bow', 'wic-bow', 'wsc-bow']
+NGPUS = 4
+of_l = []
+for i in range(NGPUS):
+    of = open(os.path.join(BASE_DIR, 'run_gpu{}.sh'.format(i)))
+    of_l.append(of)
+for i, task in enumerate(TASKS):
+    gpu_id = i % NGPUS
+    of_l[i].write('bash superglue-baselines.sh {} {}'.format(task, gpu_id))
+for i in range(NGPUS):
+    of_l[i].close()
