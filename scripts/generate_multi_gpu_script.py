ec34c334476743d90c5148531fc4a03a328dd502
Xingjian Shi <xshiab@connect.ust.hk>
2019-12-10 00:33:30 -0800

    update

--- scripts/generate_multi_gpu_script.py
+++ scripts/generate_multi_gpu_script.py
@@ -7,10 +7,10 @@ TASKS = ['commit', 'copa', 'multirc', 'record', 'rte', 'wic', 'wsc',
 NGPUS = 4
 of_l = []
 for i in range(NGPUS):
-    of = open(os.path.join(BASE_DIR, 'run_gpu{}.sh'.format(i)))
+    of = open(os.path.join(BASE_DIR, 'run_gpu{}.sh'.format(i)), 'w')
     of_l.append(of)
 for i, task in enumerate(TASKS):
     gpu_id = i % NGPUS
-    of_l[i].write('bash superglue-baselines.sh {} {}'.format(task, gpu_id))
+    of_l[gpu_id].write('bash superglue-baselines.sh {} {}\n'.format(task, gpu_id))
 for i in range(NGPUS):
     of_l[i].close()
---
+++ scripts/run_superglue/run_gpu0.sh
@@ -0,0 +1,5 @@
+bash superglue-baselines.sh commit 0
+bash superglue-baselines.sh rte 0
+bash superglue-baselines.sh commit++ 0
+bash superglue-baselines.sh commit-bow 0
+bash superglue-baselines.sh rte-bow 0
